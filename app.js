f679d22b0bbea6b9160ea7c128680a5bf69002b9
Anders Conbere <aconbere@gmail.com>
2011-01-08 18:14:42 -0800

    denormalize

--- app.js
+++ app.js
@@ -2,164 +2,189 @@ var express = require("express")
   , app = express.createServer()
   , mongodb = require("mongodb")
 
-var events_collection;
-var hostname = "127.0.0.1"
-var port = 3030
+var eventsCollection
+  , daysCollection
+  , hostname = "127.0.0.1"
+  , port = 3030
 
-app.use(express.staticProvider(__dirname + '/public'));
 
-var client = new mongodb.Db('wrack', new mongodb.Server("127.0.0.1", 27017, {}));
+app.use(express.staticProvider(__dirname + '/public'))
+
+
+var client = new mongodb.Db('wrack', new mongodb.Server("127.0.0.1", 27017, {}))
+
 client.open(function (err) {
   if (err) throw err
-  client.createCollection('events', function (err, collection) {
+  client.createCollection('events', function (err, eCollection) {
     if (err) throw err
-    events_collection = collection;
-    app.listen(port);
+    eventsCollection = eCollection
+
+    client.createCollection('days', function (err, dCollection) {
+      if (err) throw err
+      daysCollection = dCollection
+      app.listen(port)
+    })
   })
 })
 
-app.use(express.bodyDecoder());
-app.use(express.cookieDecoder());
+app.use(express.bodyDecoder())
+app.use(express.cookieDecoder())
 
 app.set('view options', {
  layout: false
-});
+})
 
 app.get("/", function (req, res) {
-  res.send('Welcome to Wrack');
-});
+  res.send('Welcome to Wrack')
+})
 
 app.get("/api.js", function (req, res) {
   res.render("api/index.ejs", { locals: { host: hostname
                                         , port: port
                                         }
-                              });
-});
+                              })
+})
 
 app.get("/track", function (req, res) {
-  var doc = JSON.parse(decodeURIComponent(req.query.data));
+  var doc = JSON.parse(decodeURIComponent(req.query.data))
 
   if(!doc.name) {
-    res.send(JSON.stringify({"error": "name is required on events"}));
+    res.send(JSON.stringify({"error": "name is required on events"}))
     return;
   }
+
+  var date = new Date()
+  var day = [date.getFullYear(), date.getMonth(), date.getDate()].join("-")
 
   var meta = { "ip": req.connection.remoteAddress
              , "referer": req.headers.referer
              , "user-agent": req.headers["user-agent"]
-             , "date": new Date()
-             };
+             , "date": date
+             , "day": [date.getFullYear(), date.getMonth(), date.getDate()].join("-")
+             }
 
-  events_collection.insert(merge(doc, {"_meta": meta}));
-  res.send("\"ok\"");
-});
+  eventsCollection.insert(merge(doc, {"_meta": meta}))
+
+  daysQ = { "event": doc.name
+          , "day": day
+          }
+  daysDoc = merge(daysQ, { "$inc": { "count": 1 }})
+
+  daysCollection.update( { event: doc.name , day: day, date: date }
+                       , { "$inc": { count: 1 }}
+                       , { upsert: true }
+                       , function () {})
+
+  res.send("\"ok\"")
+})
 
 var getEvents = function (query, callback) {
-  events_collection.find(query, function (err, cursor) {
+  eventsCollection.find(query, function (err, cursor) {
     cursor.toArray(function(err, results) {
       var events = results.map(function (r) {
-        var date = r._meta.date;
-        r._meta.date = date.getFullYear() + "-" + date.getMonth() + "-" + date.getDate();
+        r._meta.date = r._meta.date.getTime()
         return r;
       })
-      callback(err, events);
-    });
-  });
-};
+      callback(err, events)
+    })
+  })
+}
 
 var renderEventsPage = function (name, events, res) {
   res.render("events/index.haml", { locals: { events: events
                                             , name: name
                                             }
-                                  });
-};
+                                  })
+}
+
+app.get("/api/events/count", function (req, res) {
+  daysCollection.find({}, function (err, cursor) {
+    cursor.toArray(function(err, results) {
+      res.send(JSON.stringify(results))
+    })
+  })
+})
 
 app.get("/events", function (req, res) {
-  //var query = JSON.parse(req.query.q);
-  var query = {};
+  //var query = JSON.parse(req.query.q)
+  var end = new Date()
+  var start = new Date(end.getTime() - (30 * 24 * 60 * 60 * 1000))
+  var query = {"_meta.date": {"$gte": start, "$lte": end}}
+
   getEvents(query, function (err, events) {
     var name = query.name || "all"
-    renderEventsPage(name, events, res);
-  });
-});
-
-app.get("/events/:name", function (req, res) {
-  var query = merge({ name: params.name }, JSON.parse(req.query.q));
-  getEvents(query, function (err, events) {
-    renderEventsPage(query.name, events, res);
-  });
-});
+    renderEventsPage(name, events, res)
+  })
+})
 
 // gets a list of all the event names in the db
 app.get("/api/events/names", function (req, res) {
-  events_collection.distinct('name', function (err, results) {
-    res.send(JSON.stringify(results));
-  });
-});
+  eventsCollection.distinct('name', function (err, results) {
+    res.send(JSON.stringify(results))
+  })
+})
 
 // q=<query>
 app.get("/api/events", function (req, res) {
-  console.log("GET /api/events");
+  console.log("GET /api/events")
   // { <event.name> : { <date>: <count> } } 
-  var end = new Date();
-  var start = new Date(end.getTime() - (30 * 24 * 60 * 60 * 1000));
+  var end = new Date()
+  var start = new Date(end.getTime() - (30 * 24 * 60 * 60 * 1000))
   var command = { mapreduce: "events"
                 , query: {"_meta.date": {"$gte": start, "$lte": end}}
                 , map: (function () {
-                    var date =  this._meta.date;
-                    var d = date.getFullYear() + "-" + date.getMonth() + "-" + date.getDate();
-                    var val = {};
-                    val[d] = 1;
-                    emit(this.name, val);
+                    var val = {}
+                    val[this._meta.day] = 1
+                    emit(this.name, val)
                   }).toString()
                 , reduce: (function (key, values) {
                     // [ ["2010-05-01", 1 ], ... ]
-                    var datesCount = {};
+                    var datesCount = {}
                     for (var i = 0; i < values.length; i++) {
-                      var value = values[i];
-                      var keys = [];
+                      var value = values[i]
+                      var keys = []
                       for (var k in value) {
-                        keys.push(k);
+                        keys.push(k)
                       }
                       for (var j = 0; j < keys.length; j++) {
-                        var key = keys[j];
+                        var key = keys[j]
                         if (datesCount[key]) {
-                          datesCount[key] += value[key];
+                          datesCount[key] += value[key]
                         } else {
-                          datesCount[key] = value[key];
+                          datesCount[key] = value[key]
                         }
                       }
                     }
                     return datesCount;
                   }).toString()
-                };
+                }
 
   client.executeDbCommand(command, function (err, results) {
-    console.log(results);
-    var tempCollection = results.documents[0].result;
+    var tempCollection = results.documents[0].result
+    console.log(results.documents)
     client.createCollection(tempCollection, function (err, collection) {
       collection.find(function (err, cursor) {
         cursor.toArray(function (err, results) {
           res.send(JSON.stringify(results))
-        });
-      });
-    });
-  });
-});
+        })
+      })
+    })
+  })
+})
 
 app.get("/api/events/all", function (req, res) {
-  events_collection.find(JSON.parse(req.query.q), function (err, cursor) {
+  eventsCollection.find(JSON.parse(req.query.q), function (err, cursor) {
     cursor.toArray(function(err, results) {
       res.send(JSON.stringify(results))
-    });
-  });
-});
+    })
+  })
+})
 
 var merge = function (target, source) {
   for (var k in source) {
     if (source.hasOwnProperty(k)) {
-      target[k] = source[k];
+      target[k] = source[k]
     }
   }
-  return target;
-};
+  return target
+}
