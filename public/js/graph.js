27601eec992998113f5840fac4426ea7b6b7a66b
Anders Conbere <aconbere@gmail.com>
2011-01-13 15:29:30 -0800

    graphing!

--- public/js/graph.js
+++ public/js/graph.js
@@ -1,7 +1,22 @@
 $(function () {
   // get the list of all the events and their counts on days
-  $.get("/api/events/count", {}, function (data) {
-    console.log(data)
-    $.plot($("#plot"), [[1,2],[3,4]])
+  $.getJSON("/api/events/count", {}, function (data) {
+    var events = data.map(function (e) {
+      var counts = [];
+      Object.keys(e.counts).forEach(function (c) {
+        counts.push([new Date(c).getTime(), e.counts[c]]);
+      })
+      e.counts = counts;
+      return e;
+    })
+
+    var data = events.map(function (ev) {
+      return { label: ev.event, data: ev.counts}
+    })
+    $.plot($("#plot"), data, { xaxis: { mode: "time", max: (new Date().getTime() + 1000 * 60 * 60 * 24)}
+                             , series: { lines: { show: true }
+                                       , points: { show: true }
+                                       }
+                             })
   })
 })
